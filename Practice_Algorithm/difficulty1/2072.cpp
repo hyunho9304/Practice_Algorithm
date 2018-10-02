4ffff3f11393fd1b35f7fe447374f445bd5f4779
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-10-02 20:03:00 +0900

    COMMIT // initial setting

---
+++ Practice_Algorithm/difficulty1/2072.cpp
@@ -0,0 +1,12 @@
+/*
+2072. 홀수만 더하기
+https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QSEhaA5sDFAUq&categoryId=AV5QSEhaA5sDFAUq&categoryType=CODE
+*/
+#include<iostream>
+using namespace std ;
+
+int main() {
+
+	
+
+}
