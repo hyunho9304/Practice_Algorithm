c44d23f6ac70d778049f70d222cf85c39fc385fa
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-10-03 21:08:20 +0900

    COMMIT // success difficulty 1

---
+++ Practice_Algorithm/difficulty1/1545.cpp
@@ -0,0 +1,31 @@
+/*
+1545. 거꾸로 출력해 보아요
+https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV2gbY0qAAQBBAS0&categoryId=AV2gbY0qAAQBBAS0&categoryType=CODE
+
+< input >
+8
+
+< output >
+8 7 6 5 4 3 2 1 0
+
+*/
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+using namespace std ;
+
+int main() {
+
+	int inputNum = 0 ;
+	scanf("%d" , &inputNum ) ;
+
+	for( int i = inputNum ; i >= 0 ; i-- )
+		printf("%d " , i ) ;
+	printf("\n" ) ;
+
+	return 0 ;
+}
+
+
