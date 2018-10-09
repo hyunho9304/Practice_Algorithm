fed26d9ed7d070eb69609d2712f8360fd0d8fe45
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-10-09 23:45:08 +0900

    COMMIT // ~2007.cpp

---
+++ Practice_Algorithm/difficulty2/2007.cpp
@@ -0,0 +1,77 @@
+/*
+2007. 패턴 마디의 길이
+https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5P1kNKAl8DFAUq&categoryId=AV5P1kNKAl8DFAUq&categoryType=CODE
+
+< input >
+3       
+KOREAKOREAKOREAKOREAKOREAKOREA
+SAMSUNGSAMSUNGSAMSUNGSAMSUNGSA
+GALAXYGALAXYGALAXYGALAXYGALAXY       
+ 
+ 
+
+< output >
+#1 5
+#2 7
+#3 6
+ 
+ 
+ 
+ 
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std ;
+
+int main() { 
+
+	int totalNum = 0 ;
+	cin >> totalNum ;
+
+	int i = 0 ;
+	while( i < totalNum ) {
+
+		string str ;
+		cin >> str ;
+
+		char *charStr = new char[ str.length() + 1 ] ;
+		strcpy( charStr , str.c_str() ) ;
+
+		int result = 0 ;
+		for( int x = 0 , y = 1 ; y < str.length() ; y++ ) {
+
+			if( charStr[x] == charStr[y] ) {
+
+				if( x == 0 ) 	//	처음
+					result = y ;
+
+				x++ ;
+
+			} else {
+				result = 0 ;
+				x = 0 ;
+			}
+		}
+
+		cout << "#" << ( i + 1 ) << " " << result << endl ;
+		i++ ;
+	}
+
+	return 0 ;
+
+}
+
+
+
+
+
+
+
+
+
