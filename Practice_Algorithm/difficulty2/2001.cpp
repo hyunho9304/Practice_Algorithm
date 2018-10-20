a81aa2fcfaee03efb665ad37104d3c469488265b
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-10-20 21:57:35 +0900

    COMMIT // ~2001.cpp

---
+++ Practice_Algorithm/difficulty2/2001.cpp
@@ -0,0 +1,102 @@
+/*
+2001. 파리 퇴치
+https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5PzOCKAigDFAUq&categoryId=AV5PzOCKAigDFAUq&categoryType=CODE
+
+< input >
+2
+5 2
+1 3 3 6 7
+8 13 9 12 8
+4 16 11 12 6
+2 4 1 23 2
+9 13 4 7 3
+6 3
+29 21 26 9 5 8
+21 19 8 0 21 19
+9 24 2 11 4 24
+19 29 1 0 21 19
+10 29 6 18 4 3
+29 11 15 3 3 29  
+ 
+ 
+
+< output >
+#1 49
+#2 159
+ 
+ 
+ 
+ 
+*/
+
+
+#include<iostream>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std ;
+
+int main() { 
+
+	int totalNum = 0 ;
+	scanf("%d" , &totalNum ) ;
+
+	int i = 0 ;
+	while( i < totalNum ) {
+
+		int N = 0 , M = 0 ;
+		scanf("%d %d" , &N , &M ) ;
+
+		int arr[15][15] = { 0 } ;
+		for( int j = 0 ; j < N ; j++ )
+			for( int k = 0 ; k < N ; k++ )
+				scanf("%d" , &arr[j][k] ) ;
+
+		vector<int> vecSum ;
+
+		for( int j = 0 ; j <= N - M ; j++ ) {
+			for( int k = 0 ; k <= N - M ; k++ ) {
+
+				int sum = 0 ;
+				for( int a = j ; a < j + M ; a++ ) {
+					for( int b = k ; b < k + M ; b++ )
+						sum += arr[a][b] ;
+				}
+				vecSum.push_back( sum ) ;
+			}
+		}
+
+		int max = *max_element( vecSum.begin() , vecSum.end() ) ;
+
+		printf("#%d %d\n" , ( i + 1 ) , max ) ;
+		i++ ;
+	}
+
+	return 0 ;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
