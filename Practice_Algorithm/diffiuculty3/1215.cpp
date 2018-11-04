a38cd43340f541c88274818f3a700f89293ec51f
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-11-04 23:52:16 +0900

    UPDATE // 1215.cpp

--- Practice_Algorithm/diffiuculty3/1215.cpp
+++ Practice_Algorithm/diffiuculty3/1215.cpp
@@ -1,36 +1,6 @@
 /*
  1215. [S/W 문제해결 기본] 3일차 - 회문1
  https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14QpAaAAwCFAYi&categoryId=AV14QpAaAAwCFAYi&categoryType=CODE#
- 
- < input >
- 4
- CBBCBAAB
- CCCBABCB
- CAAAACAB
- BACCCCAC
- AABCBBAC
- ACAACABC
- BCCBAABC
- ABBBCCAA
- 4
- BCBBCACA
- BCAAACAC
- ABACBCCB
- AACBCBCA
- ACACBAAA
- ACCACCCB
- AACAAABA
- CACCABCB
- 
- 
- 
- < output >
- #1 12
- #2 10
- 
- 
- 
- 
  */
 
 
