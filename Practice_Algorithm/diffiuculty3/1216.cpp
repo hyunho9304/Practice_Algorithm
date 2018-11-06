0e9f4c83477a819ef99b702a24d280fd58837711
Unknown <gusgh1492@mail.hongik.ac.kr>
2018-11-06 23:52:58 +0900

    COMMIT // ~1216.cpp

---
+++ Practice_Algorithm/diffiuculty3/1216.cpp
@@ -0,0 +1,55 @@
+/*
+1216. [S/W 문제해결 기본] 3일차 - 회문2
+https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14Rq5aABUCFAYi&categoryId=AV14Rq5aABUCFAYi&categoryType=CODE
+*/
+
+#include<iostream>
+#include<vector>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+using namespace std ;
+
+int main() {
+
+    int testSize = 10 ;
+
+    int T = 0 ;
+    while( T++ < testSize ) {
+
+        scanf("%d" , &T ) ;
+
+        char arr[100][100] ;
+
+        for( int i = 0 ; i < 100 ; i++ ) {
+            for( int j = 0 ; j < 100 ; j++ )
+                scanf("%c" , &arr[i][j] ) ;
+        }
+
+        
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
